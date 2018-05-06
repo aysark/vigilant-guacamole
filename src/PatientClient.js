74c3b26198247c1599e6f63eaa3427395d3c39a1
Aysar Khalid <ak@akpro.net>
2018-05-06 08:20:47 -0700

    update token

--- src/PatientClient.js
+++ src/PatientClient.js
@@ -1,4 +1,4 @@
-const token = '5b284x7u9k8syjfmqpxhas2p';
+const token = 'y6kterb92e6fxs3u9g4fapte';
 
 function getPatient(id, cb) {
   return fetch(`https://api.athenahealth.com/preview1/1959540/patients/${id}`, {
